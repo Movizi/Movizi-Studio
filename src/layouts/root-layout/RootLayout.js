967d3c51fef64f2f886fef897a22ca72321ad876
NikaGogelia <gogelianika1@gmail.com>
2023-11-16 21:01:29 +0400

    modify root layout last content

--- src/layouts/root-layout/RootLayout.js
+++ src/layouts/root-layout/RootLayout.js
@@ -1,6 +1,7 @@
 import "./root-layout.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
+import BookCall from "../../components/book-call/BookCall";
 import Footer from "../../components/footer/Footer";
 
 function RootLayout() {
@@ -10,7 +11,10 @@ function RootLayout() {
       <main className="main-content">
         <Outlet />
       </main>
-      <Footer />
+      <div className="last-content">
+        <BookCall />
+        <Footer />
+      </div>
     </div>
   );
 }
