c311931f2387b3a365d9bf0d321dae34153a2a04
NikaGogelia <gogelianika1@gmail.com>
2023-11-16 18:43:19 +0400

    add navbar and its styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,37 @@
 import "./navbar.css";
+import { NavLink } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 
 function Navbar() {
   return (
     <nav className="navigation">
-      <h1>navbar</h1>
+      <Row className="align-items-center">
+        <Col
+          lg={4}
+          className="nav-brand d-flex align-items-center justify-content-start"
+        >
+          <img src="/Logo.svg" className="brand-logo" alt="brand-logo" />
+          <h1 className="brand-name">Movizi</h1>
+        </Col>
+        <Col
+          lg={8}
+          className="nav-links d-flex align-items-center justify-content-end"
+        >
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/projects" className="nav-link">
+            Projects
+          </NavLink>
+          <NavLink to="/process" className="nav-link">
+            Process
+          </NavLink>
+          <NavLink to="/insights" className="nav-link">
+            Insights
+          </NavLink>
+          <button className="button contact-us-button">Contact Us</button>
+        </Col>
+      </Row>
     </nav>
   );
 }
