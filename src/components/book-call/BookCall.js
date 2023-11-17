d71993398021d66f6b6ec330b554f3824e8d4abc
NikaGogelia <gogelianika1@gmail.com>
2023-11-18 01:31:37 +0400

    add button popover

--- src/components/book-call/BookCall.js
+++ src/components/book-call/BookCall.js
@@ -1,8 +1,16 @@
 import "./book-call.css";
 import { Col, Row } from "react-bootstrap";
 import MeetUsCard from "../meet-us-card/MeetUsCard";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 function BookCall() {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Copy
+    </Tooltip>
+  );
+
   return (
     <div className="book-call">
       <Row>
@@ -15,17 +23,23 @@ function BookCall() {
           </p>
           <h6>Or shoot us a mail</h6>
           <hr />
-          <button
-            className="button button-gray copy-email-button"
-            onClick={(e) =>
-              navigator.clipboard.writeText(e.currentTarget.textContent)
-            }
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 100, hide: 300 }}
+            overlay={renderTooltip}
           >
-            <span>hi@Movizi.studio</span>
-            <span>
-              <img src="/assets/icons/copy.svg" alt="copy-icon" />
-            </span>
-          </button>
+            <button
+              className="button button-gray copy-email-button"
+              onClick={(e) =>
+                navigator.clipboard.writeText(e.currentTarget.textContent)
+              }
+            >
+              <span>hi@Movizi.studio</span>
+              <span>
+                <img src="/assets/icons/copy.svg" alt="copy-icon" />
+              </span>
+            </button>
+          </OverlayTrigger>
         </Col>
         <Col lg={4} className="meet-us">
           <MeetUsCard />
