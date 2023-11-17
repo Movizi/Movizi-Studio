7fc1bd07806001ab32c6e3dcbc77547975198e7f
NikaGogelia <gogelianika1@gmail.com>
2023-11-18 01:32:29 +0400

    add parallax effect

--- src/components/home-sections/parallax/Parallax.js
+++ src/components/home-sections/parallax/Parallax.js
@@ -1,58 +1,89 @@
-import "./parallax.css";
 import { useEffect } from "react";
+import "./parallax.css";
+import Rellax from "rellax";
 
 function Parallax() {
+  useEffect(() => {
+    const rellax = new Rellax(".parallax-column");
+
+    return () => rellax.destroy();
+  }, []);
+
   return (
     <section className="parallax">
       <div className="parallax-images-container d-flex justify-content-center align-items-center">
         <div className="parallax-column-container d-flex">
-          <div className="parallax-column d-flex flex-column">
+          <div
+            className="parallax-column d-flex flex-column"
+            data-rellax-speed="4"
+          >
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
+              src="/assets/images/parallax-6.png"
+              alt="parallax-img"
+            />
+            <img
+              className="parallax-image"
               src="/assets/images/parallax-1.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-2.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-3.png"
               alt="parallax-img"
             />
           </div>
-          <div className="parallax-column d-flex flex-column">
+          <div
+            className="parallax-column d-flex flex-column"
+            data-rellax-speed="10"
+          >
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
+              src="/assets/images/parallax-6.png"
+              alt="parallax-img"
+            />
+            <img
+              className="parallax-image"
               src="/assets/images/parallax-4.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-5.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-6.png"
               alt="parallax-img"
             />
           </div>
-          <div className="parallax-column d-flex flex-column">
+          <div
+            className="parallax-column d-flex flex-column"
+            data-rellax-speed="4"
+          >
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
+              src="/assets/images/parallax-6.png"
+              alt="parallax-img"
+            />
+            <img
+              className="parallax-image"
               src="/assets/images/parallax-7.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-8.png"
               alt="parallax-img"
             />
             <img
-              className="ukiyo parallax-image"
+              className="parallax-image"
               src="/assets/images/parallax-9.png"
               alt="parallax-img"
             />
