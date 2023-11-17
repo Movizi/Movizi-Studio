ddc57e552de7199b9c5f8fbc2cacf9bdc529d712
NikaGogelia <gogelianika1@gmail.com>
2023-11-18 01:31:21 +0400

    add smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+  // Smooth Page Scrolling
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
