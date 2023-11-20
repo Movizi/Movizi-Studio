396254b22cecdea5eacb6b02f571981769d5c8dc
NikaGogelia <gogelianika1@gmail.com>
2023-11-21 01:18:42 +0400

    add projects data

---
+++ src/data/Projects.js
@@ -0,0 +1,57 @@
+export const Projects = [
+  {
+    projectId: 1,
+    projectName: "Maverick",
+    projectDescription:
+      "Maverick is design & production agency that empowers leading global brands to deliver their messages to the digital doorstep of the world's consumers.",
+    projectImg: "/assets/images/project-img-1.png",
+    projectServices: ["Design", "Development"],
+    projectType: "fashion online magazine",
+    projectLiveLink: "",
+    projectRepoLink: "",
+  },
+  {
+    projectId: 2,
+    projectName: "Maverick",
+    projectDescription:
+      "Maverick is design & production agency that empowers leading global brands to deliver their messages to the digital doorstep of the world's consumers.",
+    projectImg: "/assets/images/project-img-2.png",
+    projectServices: ["Design", "Development"],
+    projectType: "fashion online magazine",
+    projectLiveLink: "",
+    projectRepoLink: "",
+  },
+  {
+    projectId: 3,
+    projectName: "Maverick",
+    projectDescription:
+      "Maverick is design & production agency that empowers leading global brands to deliver their messages to the digital doorstep of the world's consumers.",
+    projectImg: "/assets/images/project-img-3.png",
+    projectServices: ["Design", "Development"],
+    projectType: "fashion online magazine",
+    projectLiveLink: "",
+    projectRepoLink: "",
+  },
+  {
+    projectId: 4,
+    projectName: "Studio Art ",
+    projectDescription:
+      "Maverick is design & production agency that empowers leading global brands to deliver their messages to the digital doorstep of the world's consumers.",
+    projectImg: "/assets/images/project-img-4.png",
+    projectServices: ["Design", "Development"],
+    projectType: "fashion online magazine",
+    projectLiveLink: "",
+    projectRepoLink: "",
+  },
+  {
+    projectId: 5,
+    projectName: "Lotties",
+    projectDescription:
+      "Maverick is design & production agency that empowers leading global brands to deliver their messages to the digital doorstep of the world's consumers.",
+    projectImg: "/assets/images/project-img-5.png",
+    projectServices: ["Design", "Development"],
+    projectType: "fashion online magazine",
+    projectLiveLink: "",
+    projectRepoLink: "",
+  },
+];
