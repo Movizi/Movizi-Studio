2339ba030cf7ef7692e648d879999533da398bcc
NikaGogelia <gogelianika1@gmail.com>
2023-11-25 01:52:13 +0400

    create process and approach section data

---
+++ src/data/ProcessApproachData.js
@@ -0,0 +1,71 @@
+export const ProcessApproachData = [
+  {
+    id: 1,
+    name: "Discover",
+    smallDescription:
+      "At this point we get to know your business, your goal and your target audience. We conduct research and analysis to gain insights that will be critical to the rest of the project.",
+    fullDescription:
+      "In the discovery phase, we gather information about your business, your goals, and your target audience. We conduct research to learn more about your industry, your competition, and your unique value proposition. We also work with you to determine your goals for the project, such as increasing leads, improving user experience, or driving online sales. By getting to know your business and target audience intimately, we can develop a strategy that meets your needs and delivers results.",
+    serviceList: [
+      "Defining project goals and objectives",
+      "Conducting research and analysis",
+      "Establishing user personas and scenarios",
+      "Content strategy and planning",
+      "Defining technical requirements",
+      "Developing a sitemap and information architecture",
+      "Share comprehensive project documentation",
+    ],
+  },
+  {
+    id: 2,
+    name: "Design",
+    smallDescription:
+      "Using insights from the discovery phase, we translate our ideas into visual concepts and design a user experience that meets the needs of your target audience. This includes the creation of wireframes, style guides, mockups and interactive prototypes.",
+    fullDescription:
+      "In the design phase, we bring your vision to life. Based on the insights we gained in the discovery phase, we translate our ideas into visual concepts and design a user experience that conveys your message to your audience. We also design a user experience that meets the needs of your target audience and supports your business goals. By focusing on both aesthetics and usability, we make sure your website looks good and works even better.",
+    serviceList: [
+      "UX Research",
+      "User persona creation",
+      "Information architecture",
+      "Wireframes",
+      "UI Design",
+      "Interactive prototypes",
+      "Animations",
+    ],
+  },
+  {
+    id: 3,
+    name: "Develop",
+    smallDescription:
+      "once the design is approved, our team of experienced developers brings it to life. We use the latest technologies to create a responsive, fast and functional website that meets the needs of your business.",
+    fullDescription:
+      "Once the design is approved, our team of experienced developers brings it to life. Using modern frameworks and technologies, we create a responsive, fast and functional website that meets the needs of your business. We also optimize your website for search engines and integrate it with any necessary third-party tools or platforms. Throughout the development process, we perform thorough testing to ensure that your website works perfectly on all devices and browsers.",
+    serviceList: [
+      "Front-end Development",
+      "Back-end Development",
+      "Database management",
+      "Quality Assurance and Testing",
+      "API development",
+      "Security implementation",
+      "Performance optimization",
+      "Deployment and Hosting",
+      "Maintenance and support",
+      "Documentation and training",
+    ],
+  },
+  {
+    id: 4,
+    name: "Improve",
+    smallDescription:
+      "Launching your website is just the beginning. We work with you to collect user feedback, track performance, and make data-driven improvements over time. This ensures your website stays relevant and effective in an ever-changing digital landscape.",
+    fullDescription:
+      "Launching your website is just the beginning. To ensure lasting success, we work with you to collect user feedback, track performance, and make data-driven improvements over time. This includes monitoring website analytics to identify areas for improvement, conducting user testing to gather feedback on the user experience, and updating your website content and features as needed. By continuously optimizing your website, we help you stay ahead of the competition and achieve your business goals.",
+    serviceList: [
+      "Analytics and Data Analysis",
+      "Project upgrades",
+      "Content Optimization",
+      "Accessibility Optimization",
+      "Maintenance and Support",
+    ],
+  },
+];
